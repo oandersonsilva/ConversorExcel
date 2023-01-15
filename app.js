326b45cde7d3d463d7b345213a745ab6e88f8dcc
oandersonsilva <oandersonsouza.dev@gmail.com>
2023-01-14 21:50:40 -0300

    gerador HTML

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+const HtmlParser = require('./HtmlParser')
 const Processor = require('./Processor')
 const Reader = require('./Reader')
+const Table = require('./Table')
+const Writer = require('./Writer')
 
 var leitor = new Reader()
+var escritor = new Writer()
 
 async function main() {
   var dados = await leitor.Read('./arquivo.csv')
   console.log(dados)
   var linhas = Processor.process(dados)
-  //titulo
-  console.log(linhas[0])
-  //demais linhas
-  linhas.shift()
-  console.log(linhas)
+
+  var usuarios = new Table(linhas)
+
+  var html = await HtmlParser.Parse(usuarios)
+  console.log(html)
+
+  escritor.Write(Date.now() + '.html', html)
 }
 
 main()
